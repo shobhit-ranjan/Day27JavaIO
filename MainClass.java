6bc367885f5043a86927760fcec46659ebd2a9c2
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-27 07:50:02 +0530

    Upadated file stru.

--- MainClass.java
+++ MainClass.java
@@ -3,21 +3,26 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
 public class MainClass {
 
 	public static void main(String[] args) throws IOException {
 
 		Employeesystem.showUser();
-		
+
 		Path path = Paths.get("C:\\Users\\shobh\\eclipse-workspace\\Day27JavaIO\\src\\filedemo.txt");
-        List<String> list = Files.readAllLines(path);
-        System.out.println(list);
-        
+		Path dirpath = Paths.get("C:\\Users\\shobh\\eclipse-workspace\\Day27JavaIO\\src");
+		List<String> list = Files.readAllLines(path);
+		System.out.println(list);
+
 		Files.deleteIfExists(path);
-		Files.createFile(path );
-        System.out.println(Files.exists(path));
-        
-        Files.walk(path).filter(Files::isRegularFile).forEach(System.out::println);
+		Files.createFile(path);
+		Files.createDirectories(dirpath);
+		Files.createFile(dirpath);
+
+		System.out.println(Files.exists(path));
+
+		Files.walk(path).filter(Files::isRegularFile).forEach(System.out::println);
 	}
 
 }
